33277b7e02284be3b710360105a8cd5aea908a10
Juan Solares <solaresjuan43@gmail.com>
2021-06-23 11:53:08 -0600

    commit

--- src/app/reactive/basics/basics.component.ts
+++ src/app/reactive/basics/basics.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,7 +7,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styles: [
   ]
 })
-export class BasicsComponent {
+export class BasicsComponent implements OnInit {
 
   // myForm: FormGroup = new FormGroup({
   //   name: new FormControl('iPhone 12'),
@@ -17,12 +17,33 @@ export class BasicsComponent {
 
   constructor(private formBuilder: FormBuilder) { }
 
+  ngOnInit() {
+    this.myForm.reset({
+      name: 'iPhone 12',
+      price: 899,
+      existences: 45
+    })
+  }
+
   myForm: FormGroup = this.formBuilder.group({
-    name: ['iPhone 12', [Validators.required, Validators.minLength(3)]],
-    price: [0, [Validators.required, Validators.min(0)]],
-    existences: [0, [Validators.required, Validators.min(0)]]
+    name: [, [Validators.required, Validators.minLength(3)]],
+    price: [, [Validators.required, Validators.min(0)]],
+    existences: [, [Validators.required, Validators.min(0)]]
   })
 
 
+  validField(field: string) {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
 
+  save() {
+
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 }
--- src/app/reactive/dynamics/dynamics.component.ts
+++ src/app/reactive/dynamics/dynamics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamics',
@@ -6,11 +7,52 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DynamicsComponent implements OnInit {
 
-  constructor() { }
+export class DynamicsComponent {
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) { }
+
+  myForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favorites: this.formBuilder.array(
+      [
+        ['Super Mario Bros', Validators.required ], 
+        ['Fifa 21', Validators.required]], 
+        Validators.required)
+  });
+
+  newFavorite: FormControl = this.formBuilder.control('', Validators.required);
+
+  get favoritesArr() {
+    return this.myForm.get('favorites') as FormArray;
   }
 
+  validField(field: string) {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+
+  }
+
+  save() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
+
+  addFavorite() {
+    if(this.newFavorite.invalid){
+      return;
+    }
+
+    //this.favoritesArr.push( new FormControl(this.newFavorite.value, Validators.required));
+    this.favoritesArr.push( this.formBuilder.control(this.newFavorite.value, Validators.required));
+    this.newFavorite.reset();
+  }
+
+  delete(index: number) {
+    this.favoritesArr.removeAt(index);
+  }
 }
